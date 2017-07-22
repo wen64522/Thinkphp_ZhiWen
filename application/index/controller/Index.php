2272ce885033c9f340e6a419c393136ca53482fe
wenyiwen <645228905@qq.com>
2017-07-22 15:47:30 +0800

    完成注册功能

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -3,10 +3,25 @@ namespace app\index\controller;
 
 use think\Controller;
 
+use think\Db;
 class Index extends Controller
 {
     public function index()
     {
         return $this->fetch();
+    }
+    public function add_user(){
+        sleep(2);
+        $t=time();
+        $data= [
+            'name'=>$_POST['user'],
+            'password'=>$_POST['password'],
+            'email'=>$_POST['email'],'sex'=>$_POST['sex'],
+            'birthday'=>$_POST['birthday'],
+            'date'=>$t,
+        ];
+        Db::name('user')->insert($data);
+
+        return true;
     }
 }
